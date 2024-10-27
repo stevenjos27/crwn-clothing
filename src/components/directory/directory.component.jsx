3708f38c6540801d0d3518fc9e7928ff2acba0a6
HDFC <hdfc@HDFCs-MacBook-Pro.local>
2024-10-27 13:24:54 +0530

    test

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,6 +3,7 @@ import CategoryItem from "../category-item/category-item.component";
 const Directory = ({categories}) => {
     return (
         <div className="directory-container">
+            <h1>Test</h1>
             {categories.map((category) => (
                 <CategoryItem key={category.id} category={category}/>
             ))}
