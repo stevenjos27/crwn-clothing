c06c34a35ca0af30c951ef477ebdceb8f75e648b
Steven Joseph <stevenj27@outlook.com>
2024-11-17 16:18:45 +0530

    cart and checkout added

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -9,11 +9,10 @@ const CategoryItem = ({ category }) => {
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
-      >
-        <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
       </div>
     </div>
   );
