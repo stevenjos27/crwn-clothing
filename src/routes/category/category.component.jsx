2e0ceb8653a01927e3b86566f53e84741565f6fc
Steven Joseph <stevenj27@outlook.com>
2025-03-02 15:27:11 +0530

    api response data generalized

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -9,10 +9,12 @@ import { selectCategoriesMap } from '../../store/categories/category.selector';
 
 const Category = () => {
     const { category } = useParams();
+    console.log('render/re-rendering of Category component');
     const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
+        console.log('useEffect fired of Category component');
         setProducts(categoriesMap[category]);
     }, [category, categoriesMap]);
 
